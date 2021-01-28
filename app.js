b1c878c45aefeff364e606e3839d52b01ba6f2ca
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2021-01-27 21:10:18 -0600

    Add arrays: heroes, cities, creatures

---
+++ app.js
@@ -0,0 +1,3 @@
+const heroes = ['Jason', 'Heracles', 'Perseus', 'Theseus', 'Achilles', 'Greg'];
+const cities = ['Thebes', 'Ephesus', 'Sparta', 'Corinth', 'Argos', 'Los Angeles'];
+const creatures = ['minotaur', 'hydra', 'poodle', 'chimera', 'clownfish', 'rabbit'];
