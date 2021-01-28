a9a188c1dcf11c1f7f628e00010013c0e82418ea
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2021-01-28 09:04:41 -0600

    Create random number generator

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const heroes = ['Jason', 'Heracles', 'Perseus', 'Theseus', 'Achilles', 'Greg'];
 const cities = ['Thebes', 'Ephesus', 'Sparta', 'Corinth', 'Argos', 'Los Angeles'];
 const creatures = ['minotaur', 'hydra', 'poodle', 'chimera', 'clownfish', 'rabbit'];
+
+const numGenerator = numOptions => Math.floor(Math.random() * numOptions);
