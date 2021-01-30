6947e936673185012a1eacb8ccf2230327b9233d
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2021-01-29 21:12:25 -0600

    Add formatMyth and generateMyth functions, relocate template

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const creatures = ['minotaur', 'hydra', 'poodle', 'chimera', 'pgymy goat', 'rabb
 
 const numGenerator = numOptions => Math.floor(Math.random() * numOptions);
 
-const template = "The great hero HERO was traveling near the city of CITY when he heard \
-that the city was being menaced by a vicious CREATURE. The CREATURE would lurk at the outskirts \
-of the city and eat anyone who dared to come near. But HERO was not deterred and set out to \
-confront the CREATURE. They fought a great battle! But through his strength and cunning HERO was \
-able to slay the beast. The citizens of CITY were grateful and presented HERO with a cloak of the \
-CREATURE's hide to wear. To this day the area around CITY is sometimes called the land of HERO.";
+const formatMyth = () => {
+    const hero = heroes[numGenerator(heroes.length)];
+    const city = cities[numGenerator(cities.length)];
+    const creature = creatures[numGenerator(creatures.length)];
+    const template = `The great hero ${hero} was traveling near the city of ${city} when he heard \
+that the city was being menaced by a vicious ${creature}. The ${creature} would lurk at the outskirts \
+of the city and eat anyone who dared to come near. But ${hero} was not deterred and set out to \
+confront the ${creature}. They fought a great battle! But through his strength and cunning ${hero} was \
+able to slay the beast. The citizens of ${city} were grateful and presented ${hero} with a cloak of the \
+${creature}'s hide to wear. To this day the area around ${city} is sometimes called the land of ${hero}.`;
+    return template;
+};
+
+const generateMyth = () => {
+    // will be used to pick a template when more than 1 is available
+    console.log(formatMyth());
+};
+
+generateMyth();
