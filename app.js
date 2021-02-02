7c42ddce4162330bbd79f5d5abb6685bf70e4f6f
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2021-02-01 22:48:05 -0600

    Finish & rename immortality myth; wrap stories to 80 chars

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ const gods = [
     {name: 'Hermes', pronoun: ['he', 'him'], possessive: 'his'}
 ];
 const mortals = [
-    {},
-    {},
-    {}
+    {name: 'Hilarion', pronoun: ['he', 'him'], possessive: 'his'},
+    {name: 'Euanthe', pronoun: ['she', 'her'], possessive: 'her'},
+    {name: 'Korinna', pronoun: ['she', 'her'], possessive: 'her'},
+    {name: 'Kallias', pronoun: ['he', 'him'], possessive: 'his'}
 ];
+const instruments = ['kithara', 'aulos flute', 'Casio CT-S200 keyboard', 'bongo drum'];
 
 const numGenerator = numOptions => Math.floor(Math.random() * numOptions);
 
@@ -23,13 +25,15 @@ const creatureMyth = () => {
     const hero = heroes[numGenerator(heroes.length)];
     const city = cities[numGenerator(cities.length)];
     const creature = creatures[numGenerator(creatures.length)];
-    const template = `${hero} and the ${creature}\n\n\
+    const template = `\n${hero} and the ${creature}\n\n\
 The great hero ${hero} was traveling near the city of ${city} when he heard \
-that the city was being menaced by a vicious ${creature}. The ${creature} would lurk at the outskirts \
-of the city and eat anyone who dared to come near. But ${hero} was not deterred and set out to \
-confront the ${creature}. They fought a great battle! But through his strength and cunning ${hero} was \
-able to slay the beast. The citizens of ${city} were grateful and presented ${hero} with a cloak of the \
-${creature}'s hide to wear. To this day the area around ${city} is sometimes called the land of ${hero}.`;
+that the city was being menaced by a vicious ${creature}. The ${creature} \
+would lurk at the outskirts of the city and eat anyone who dared to come near. \
+But ${hero} was not deterred and set out to confront the ${creature}. They \
+fought a great battle! But through his strength and cunning ${hero} was able to \
+slay the beast. The citizens of ${city} were grateful and presented ${hero} \
+with a cloak of the ${creature}'s hide to wear. To this day the area around \
+${city} is sometimes called the land of ${hero}.`;
     return template;
 };
 
@@ -37,20 +41,37 @@ const plagueMyth = () => {
     const god = gods[numGenerator(gods.length)];
     const city = cities[numGenerator(cities.length)];
     const plague = plagues[numGenerator(plagues.length)];
-    const template = `The plague of ${plague}\n\n\
-The king of ${city} once angered ${god.name} by refusing to make sacrifices to ${god.pronoun[1]}. \
-"${god.name} is a powerless god" he declared, "and ${god.possessive} altar will see no offerings from \
-us." As punishment, ${god.name} sent a plague of ${plague} against the city. For three weeks the plague raged, and \
-the citizens of ${city} beseeched the king to change his mind. Finally, realizing his hubris, the king ordered \
-sacrifices to be made. At once the plague ceased - no trace of ${plague} could be found in the city. Ever after \
-${city} held a yearly festival to honor the power of ${god.name}, ${god.pronoun[0]} who sends ${plague}.`;
+    const template = `\nThe plague of ${plague}\n\n\
+The king of ${city} once angered ${god.name} by refusing to make sacrifices \
+to ${god.pronoun[1]}. "${god.name} is a powerless god" he declared, "and \
+${god.possessive} altar will see no offerings from us." As punishment, \
+${god.name} sent a plague of ${plague} against the city. For three weeks the \
+plague raged, and the citizens of ${city} beseeched the king to chang0e his \
+mind. Finally, realizing his hubris, the king ordered sacrifices to be made. \
+At once the plague ceased - no trace of ${plague} could be found in the city. \
+Ever after ${city} held a yearly festival to honor the power of ${god.name}, \
+${god.pronoun[0]} who sends ${plague}.`;
     return template;
 };
 
-const giftOfImmortalityMyth = () => {
+const immortalityMyth = () => {
     const god = gods[numGenerator(gods.length)];
     const mortal = mortals[numGenerator(mortals.length)];
-    const template = ``;
+    const instrument = instruments[numGenerator(instruments.length)];
+    const template = `\n${god.name} and ${mortal.name}\n\n
+The god ${god.name} once fell in love with a mortal, ${mortal.name}, who was \
+renowned not only for ${mortal.possessive} beauty but also for \
+${mortal.possessive} talent at playing the ${instrument}. Afraid to \
+lose ${mortal.name}, ${god.name} begged Zeus to grant ${mortal.pronoun[1]} \
+immortality. But Zeus was loathe to so easily give away immortality, the \
+greatest gift of the gods, and declared that he would not. He would only, he \
+said, grant immortality to a musician whose music could move the gods, one \
+whose music was worthy of being played for generations to come. Hearing this, \
+${god.name} knew that Zeus could be persuaded and brought ${mortal.name} to \
+Olympus. Standing before the gods, ${mortal.name} picked up \
+${mortal.possessive} ${instrument} and began to play. From the first \
+note all the gods were enthralled! When the song ended, Zeus reversed his \
+decision and immortalized ${mortal.name}, who became a minor deity of music.`;
     return template;
 };
 
@@ -61,8 +82,9 @@ const generateMyth = () => {
     } else if (choice === 1) {
         console.log(plagueMyth());
     } else {
-        console.log(giftOfImmortalityMyth());
+        console.log(immortalityMyth());
     }
 };
 
-generateMyth();
+//generateMyth();
+console.log(immortalityMyth());
